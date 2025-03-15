9212fb57794c9dff0a5a23f290f834d18cc16170
Louis Acresti <louis.acresti@gmail.com>
2025-03-15 16:01:41 -0700

    main = no frills

--- eslint.config.js
+++ eslint.config.js
@@ -1,7 +1,6 @@
 import globals from 'globals';
 import pluginJs from '@eslint/js';
 import tseslint from 'typescript-eslint';
-import reactHooks from 'eslint-plugin-react-hooks';
 
 export default [
   {files: ['**/*.{js,mjs,cjs,ts}']},
@@ -9,9 +8,7 @@ export default [
   pluginJs.configs.recommended,
   ...tseslint.configs.recommended,
   {
-    plugins: {
-      'react-hooks': reactHooks,
-    },
+    plugins: {},
     rules: {
       '@typescript-eslint/no-unused-vars': [
         'error',
@@ -20,8 +17,6 @@ export default [
           varsIgnorePattern: '^_',
         },
       ],
-      'react-hooks/rules-of-hooks': 'error',
-      'react-hooks/exhaustive-deps': 'error',
     },
   },
 ];
