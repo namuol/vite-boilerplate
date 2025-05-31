556189bf3d87e23a838d7d23a771f15974992ded
Louis Acresti <louis.acresti@gmail.com>
2025-05-31 08:16:26 -0700

    add import/export sorting

--- eslint.config.js
+++ eslint.config.js
@@ -1,10 +1,20 @@
 // @ts-check
 
 import eslint from '@eslint/js';
+import simpleImportSortPlugin from 'eslint-plugin-simple-import-sort';
 import tseslint from 'typescript-eslint';
 
 export default tseslint.config(
   eslint.configs.recommended,
   tseslint.configs.recommended,
   {ignores: ['dist']},
+  {
+    plugins: {
+      'simple-import-sort': simpleImportSortPlugin,
+    },
+    rules: {
+      'simple-import-sort/imports': 'warn',
+      'simple-import-sort/exports': 'warn',
+    },
+  },
 );
