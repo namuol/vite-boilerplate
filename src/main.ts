277a031b4f20b8956d85c8310f4ed7d076a4cded
Louis Acresti <louis.acresti@gmail.com>
2025-08-16 10:08:39 -0700

    hello triangle

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,69 @@
-const el = document.createElement('h1');
-el.innerText = 'Hello, world!';
-document.body.appendChild(el);
+import redFragWGSL from './red.frag.wgsl';
+import triangleVertWGSL from './triangle.vert.wgsl';
+
+const canvas = document.querySelector('canvas') as HTMLCanvasElement;
+const adapter = await navigator.gpu?.requestAdapter({
+  featureLevel: 'compatibility',
+});
+const device_ = await adapter?.requestDevice();
+if (!device_) {
+  alert('Failed to get WebGPU device. Is WebGPU supported in your browser?');
+  throw new Error('Failed to get WebGPU device');
+}
+const device = device_;
+
+const context = canvas.getContext('webgpu') as GPUCanvasContext;
+const presentationFormat = navigator.gpu.getPreferredCanvasFormat();
+
+context.configure({
+  device,
+  format: presentationFormat,
+});
+
+const pipeline = device.createRenderPipeline({
+  layout: 'auto',
+  vertex: {
+    module: device.createShaderModule({
+      code: triangleVertWGSL,
+    }),
+  },
+  fragment: {
+    module: device.createShaderModule({
+      code: redFragWGSL,
+    }),
+    targets: [
+      {
+        format: presentationFormat,
+      },
+    ],
+  },
+  primitive: {
+    topology: 'triangle-list',
+  },
+});
+
+function frame() {
+  const commandEncoder = device.createCommandEncoder();
+  const textureView = context.getCurrentTexture().createView();
+
+  const renderPassDescriptor: GPURenderPassDescriptor = {
+    colorAttachments: [
+      {
+        view: textureView,
+        clearValue: [0, 0, 0, 0], // Clear to transparent
+        loadOp: 'clear',
+        storeOp: 'store',
+      },
+    ],
+  };
+
+  const passEncoder = commandEncoder.beginRenderPass(renderPassDescriptor);
+  passEncoder.setPipeline(pipeline);
+  passEncoder.draw(3);
+  passEncoder.end();
+
+  device.queue.submit([commandEncoder.finish()]);
+  requestAnimationFrame(frame);
+}
+
+requestAnimationFrame(frame);
--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,1 +1,5 @@
+/// ```
 /// <reference types="vite/client" />
+/// <reference types="vite-plugin-glsl/ext" />
+/// <reference types="@webgpu/types" />
+/// ```
